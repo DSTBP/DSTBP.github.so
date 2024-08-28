b06e9cbbee60aa2db1c87ba1c000fe5f63e8326c
DSTBP <93864880+DSTBP@users.noreply.github.com>
2024-08-28 18:06:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -87,12 +87,12 @@ export default defineSiteConfig({
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
     },
-    // {
-    //   name: 'Travelling',
-    //   link: 'https://www.travellings.cn/go.html',
-    //   icon: 'i-ri-train-line',
-    //   color: 'var(--va-c-text)',
-    // },
+    {
+      name: '语雀',
+      link: 'https://www.yuque.com/dstbp',
+      icon: 'i-ri-yuque-line',
+      color: '#3BD47F',
+    },
   ],
 
   search: {
