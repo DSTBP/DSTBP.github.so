2a4471c2a7af8ffd77b3a2b8ddd8b35dd0bd6db1
r0 <taddeowang123@gmail.com>
2024-08-29 14:54:20 +0800

    update

---
+++ components/YunFooter.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description: 
+ * @Author: DSTBP
+ * @Date: 2024-08-29 14:49:59
+ * @LastEditTime: 2024-08-29 14:51:12
+ * @LastEditors: DSTBP
+-->
+<script lang="ts" setup>
+import { useScriptTag } from '@vueuse/core'
+import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
+
+useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
+</script>
+
+<template>
+    <YunFooter>
+        <!-- 自定义页脚内容 -->
+        <div>本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
+        <div>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
+    </YunFooter>
+</template>
---
+++ components/YunPostMeta.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Description: 
+ * @Author: DSTBP
+ * @Date: 2024-08-29 14:53:09
+ * @LastEditTime: 2024-08-29 14:53:21
+ * @LastEditors: DSTBP
+-->
+<script lang="ts" setup>
+import type { Post } from 'valaxy'
+import { useLayout } from 'valaxy'
+import YunPostMeta from 'valaxy-theme-yun/components/YunPostMeta.vue'
+
+defineProps<{
+    frontmatter: Post
+}>()
+
+// 仅在 Post 布局显示
+const isPost = useLayout('post')
+</script>
+
+<template>
+    <YunPostMeta :frontmatter="frontmatter">
+        <span v-if="isPost" id="busuanzi_container_page_pv">
+            本文总阅读量 <span id="busuanzi_value_page_pv" /> 次
+        </span>
+    </YunPostMeta>
+</template>
